26ebcc6e03cf133b71782e123c3e7c713af74ad8
Krishna Teja <s533708@nwmissouri.edu>
2019-02-01 01:16:13 -0600

    cboAlg1

--- SortComparison/frmMain.cs
+++ SortComparison/frmMain.cs
@@ -30,6 +30,7 @@ namespace SortComparison
         private void Form1_Load(object sender, EventArgs e)
         {
             this.tbSamples.Value = 800;
+            this.cboAlg1.SelectedIndex = 3;
         }
 
         public void Randomize(IList list)
